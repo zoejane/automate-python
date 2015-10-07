13d3c5ea77a069332f8f88fcdcb920c84526ef90
Zoe <dadac123@gmail.com>
2015-10-07 21:15:07 +0800

    functions

---
+++ hello-ex.py
@@ -0,0 +1,10 @@
+def hello(name):
+    print('Howdy!')
+    print('Howdy!!!')
+    print('Hello there.')
+    print('Hello '+name)
+
+print('Please input your name:')
+name=input()
+
+hello(name)
---
+++ variables_ex.py
@@ -0,0 +1,8 @@
+spam = 42 #global variable
+
+def eggs():
+    print(spam)
+    spam =420 # local variable
+
+print('Some code here.')
+print('Some more code.')
