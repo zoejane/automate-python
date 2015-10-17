974384df862f6920c5a7cdbf1afdd112089bf273
Zoe <dadac123@gmail.com>
2015-10-17 10:52:08 +0800

    open and click web

---
+++ selenuim_ex.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+browser=webdriver.Firefox()
+browser.get('https://automatetheboringstuff.com')
+elem=browser.find_element_by_css_selector('.entry-content > ol:nth-child(15) > li:nth-child(1) > a:nth-child(1)')
+elem.click()
+elems=browser.find_elements_by_css_selector('p')
+print(len(elems))
