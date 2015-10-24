9c4e8ce69da21dc58e4fc85604d9e1fc848d8c3e
Zoe <dadac123@gmail.com>
2015-10-24 21:14:09 +0800

    click mouse

--- control-the-mouse.py
+++ control-the-mouse.py
@@ -6,3 +6,10 @@ print(pyautogui.position())
 pyautogui.moveTo(10,10)
 pyautogui.moveTo(10,10,duration=1.5)
 
+pyautogui.moveRel(200,0,duration=2)
+
+print(pyautogui.position())
+pyautogui.click(339,38)
+
+pyautogui.doubleClick(339,38)
+pyautogui.rightClick(339,38)
